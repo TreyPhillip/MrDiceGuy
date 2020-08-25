3ea38620eaeacd9c6299cd339e32934be81c3882
Trey <treyphillip@live.com>
2020-08-25 02:47:02 -0400

    changed dice commands to be dynamic

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { prefix, prefix } = require('./config.json');
+const { token, prefix } = require('./config.json');
 const fs = require('fs');
 
 client.commands = new Discord.Collection();
@@ -22,13 +22,26 @@ client.on('message', (message) => {
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 	var diceRegex = /[1-9]?[1-9]?[1-9]?d[1-9][1-9]?[1-9]?/;
-	if (command === 'ping') {
-		client.commands.get('ping').execute(message, args);
-	} else if (command == 'youtube') {
-		client.commands.get('youtube').execute(message, args);
-	} else if (diceRegex.exec(command)) {
+
+	if (diceRegex.exec(command)) {
 		client.commands.get('dice').execute(message, args, command);
+	} else {
+		if (!client.commands.has(command)) return;
+
+		try {
+			client.commands.get(command).execute(message, args);
+		} catch (error) {
+			console.error(error);
+			message.reply('there was an error trying to execute that command!');
+		}
 	}
+	// if (command === 'ping') {
+	// 	client.commands.get('ping').execute(message, args);
+	// } else if (command == 'youtube') {
+	// 	client.commands.get('youtube').execute(message, args);
+	// } else if (diceRegex.exec(command)) {
+	// 	client.commands.get('dice').execute(message, args, command);
+	// }
 });
 
 client.login(token);
