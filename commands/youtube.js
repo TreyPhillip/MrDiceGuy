362a213879ac27b26bae65f80fac43a312f536ba
Trey <treyphillip@live.com>
2020-10-19 17:30:58 -0400

    remove command from testing

--- commands/youtube.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'youtube',
-	description: 'This is a youtube command',
-	execute(message, args) {
-		message.channel.send('https://www.youtube.com/user/Dracos174');
-	}
-};
